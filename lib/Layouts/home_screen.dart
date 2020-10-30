79136e3c9c110f1c24a152ef02d36685aeb11264
mkzref <marwahatamleh2@gmail.com>
2020-10-30 20:19:42 +0200

    changed

--- lib/Layouts/home_screen.dart
+++ lib/Layouts/home_screen.dart
@@ -15,28 +15,80 @@ class myapps extends StatelessWidget {
               color: Colors.lightBlueAccent[100],
               child: Center(
                   child: Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(left: 10, top: 10),
-                      child:Column(  
-                        children: [
-                          Expanded(child: Text(
-                            "Hello",
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                                fontSize: 25,
-                                fontFamily: 'Trispace',
-                                color: Colors.amber),
-                          )),
-                          Expanded(child: Text(
-                            "Hello",
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                                fontSize: 25,
-                                fontFamily: 'Trispace',
-                                color: Colors.amber),
-                          ))
-                       ],)
-                      ))),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 10, top: 10),
+                    child: Column(
+                      children: [
+                        Expanded(
+                            child: Text(
+                              "Hello",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontFamily: 'Trispace',
+                                  color: Colors.amber),
+                            )),
+                        Expanded(
+                            child: Text(
+                              "Hello",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontFamily: 'Trispace',
+                                  color: Colors.amber),
+                            )),
+                        MyImage(),
+                        MyButton(),
+                      ],
+                    ),
+
+
+                  ))),
         ));
   }
+}
+
+class MyImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    AssetImage i = new AssetImage('images/pic.jpg');
+    Image myimage = new Image(image: i);
+    return Container(child: myimage,);
+  }
+
+
+}
+
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    RaisedButton button = new RaisedButton(onPressed: () {onClick(context);},
+      color: Colors.brown,
+      child: Text("Sumbit", style: TextStyle(color: Colors.white),),
+
+    );
+    return Container(child: button,);
+
+
+  }
+void onClick(BuildContext context){
+    AlertDialog dialog = new AlertDialog(
+      title:Text( "Hello"),
+      content: Text("Welcome"),
+
+    );
+    showDialog(context: context ,
+    builder: (BuildContext context){
+      return dialog;
+
+    }
+    );
+
+
+
+}
+
 }
